65408851d7b83eec9888be60a13ed9fc700c1967
野上信悟 <nogamishingo@MacBook-Air-for-Shingo-Nogami.local>
2024-11-08 19:04:34 +0900

    3日目午後

--- src/List.js
+++ src/List.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export class List extends React.Component {
+    componentWillUnmount(){
+        console.log('List.js:unmount');
+    }
     render(){
         const {langs} = this.props;
         return(
